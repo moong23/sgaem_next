f840a51e971bb24267947633b6c507b5ed5f337a
moong <moonki0623@naver.com>
2023-05-27 14:49:14 +0900

    [feat]: Topbar Component Implement

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,18 +1,59 @@
 import Image from "next/image";
+import Link from "next/link";
+
+import TopbarMenuList from "../../constants/topbarMenuList";
 
 const Topbar = () => {
   return (
     <div className="w-full h-[5rem] flex items-center justify-center bg-red-200">
-      <div className="w-[62.5vw] h-full bg-yellow-200 flex flex-row justify-between items-center">
-        <div
-          className={"flex w-[50px] h-[45px] flex-row items-center relative"}
+      {/* Responsive: min width needed */}
+      <div className="w-[max(62.5vw,75rem)] h-full flex flex-row gap-12 justify-between items-center bg-red-400">
+        {/* App Logo Divider */}
+        <Link
+          href="/"
+          className={
+            "flex w-[4.375rem] h-[3.125rem] flex-row items-center relative"
+          }
         >
           <Image
             src="/images/applogo_black.svg"
             alt="sgaemLogo"
             fill
-            style={{ objectFit: "cover" }}
+            priority
+            style={{ objectFit: "contain" }}
           />
+        </Link>
+        {/* Topbar Menu Divider */}
+        <div
+          className={
+            "w-full h-full flex flex-row items-center gap-8 text-black font-semibold"
+          }
+        >
+          {TopbarMenuList.map((item, index) => {
+            return (
+              <Link
+                className="h-full flex items-center"
+                href={item.link}
+                key={index}
+              >
+                <span>{item.name.toUpperCase()}</span>
+              </Link>
+            );
+          })}
+        </div>
+        {/* Auth Control Divider */}
+        <div
+          className={
+            "flex flex-row w-[15%] h-full justify-end items-center gap-8"
+          }
+        >
+          {/* TODO: Auth 여부에 따른 분기처리 필요 */}
+          <Link href="/login">
+            <span>로그인</span>
+          </Link>
+          <Link href="/signup">
+            <span>회원가입</span>
+          </Link>
         </div>
       </div>
     </div>
---
+++ src/constants/TopbarMenuList.js
@@ -0,0 +1,28 @@
+const TopbarMenuList = [
+  {
+    name: "AboutUs",
+    link: "aboutus",
+  },
+  {
+    name: "competition",
+    link: "competition",
+  },
+  {
+    name: "recruitment",
+    link: "recruitment",
+  },
+  {
+    name: "courses",
+    link: "courses",
+  },
+  {
+    name: "FAQ",
+    link: "faq",
+  },
+  {
+    name: "contact us",
+    link: "contact",
+  },
+];
+
+export default TopbarMenuList;
