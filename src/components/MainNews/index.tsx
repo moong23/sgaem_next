c364e660ca8519c6a37421f7070f155cad054ba7
moong <moonki0623@naver.com>
2023-05-30 00:29:51 +0900

    [feat]: News Component implemented

--- src/components/MainNews/index.tsx
+++ src/components/MainNews/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const NewsCard = ({ news }: any) => {
   // TODO: 여기서부터 다시 작업
   return (
-    <div className="w-[30vw] h-[30vw] flex flex-col shadow-md">
+    <div className="w-[25vw] h-[30vw] flex flex-col shadow-md">
       {/* Image Container */}
       <div className="w-full h-3/4 relative">
         <Image
@@ -15,11 +15,11 @@ const NewsCard = ({ news }: any) => {
         />
       </div>
       {/* Context Container */}
-      <div className="w-full h-1/4 bg-white p-[1.25vw]">
-        <div className="text-[1.250vw] font-semibold text-[#707070]">
+      <div className="w-full h-1/4 bg-white p-[1vw]">
+        <div className="text-[1.250vw] font-semibold text-[#707070] mb-1">
           {news.headline}
         </div>
-        <div className="text-[0.833vw] font-normal text-[black] text-ellipsis-multi-line">
+        <div className="text-[0.833vw] font-normal text-[#707070] text-ellipsis-multi-line">
           {news.content}
         </div>
       </div>
@@ -32,7 +32,7 @@ const MainNews = ({ newsList }: INewsList) => {
   return (
     <>
       <div className="text-[2.083vw] font-semibold mb-1">NEWS</div>
-      <div className="text-[1.15vw] font-normal text-[#979797]">
+      <div className="text-[1.15vw] font-normal text-[#979797] mb-5">
         스겜의 최신 소식을 확인하세요.
       </div>
       <div className="w-full flex flex-row gap-8 justify-center">
