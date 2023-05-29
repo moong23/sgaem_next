0b316caf386118be55aff77d545dfc2f3142ea4b
moong <moonki0623@naver.com>
2023-05-30 01:20:30 +0900

    [WIP]: Youtube Container 작업중
    TODO : Youtube 영상 container 제작 / Youtube Next에 최적화해서 재생

---
+++ src/components/MainYoutube/index.tsx
@@ -0,0 +1,14 @@
+const MainYoutube = ({ youtubeList }: any) => {
+  console.log(youtubeList);
+  return (
+    <>
+      <div className="text-[2.083vw] font-semibold mb-1">YOUTUBE</div>
+      <div className="text-[1.15vw] font-normal text-[#979797] mb-5">
+        스겜의 활동을 영상으로 확인하세요.
+      </div>
+      {/* TODO: Youtube Container 만들기~!!! */}
+    </>
+  );
+};
+
+export default MainYoutube;
